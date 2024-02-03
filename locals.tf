915158425ab009baedc878216e6f4625081b3089
signorrayan <signor.rayan@gmail.com>
2024-02-03 22:13:09 +0100

    refactor(locals): Formatted locals.tf.

--- locals.tf
+++ locals.tf
@@ -2,6 +2,6 @@ locals {
   tags = {
     created_by = "terraform"
   }
-  account_id = data.aws_caller_identity.current.account_id
+  account_id  = data.aws_caller_identity.current.account_id
   aws_ecr_url = "${local.account_id}.dkr.ecr.${var.region}.amazonaws.com"
 }

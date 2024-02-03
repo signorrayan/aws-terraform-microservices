2f1fd56763445e6a990126ad3098dd4edc2231ce
signorrayan <signor.rayan@gmail.com>
2024-02-03 21:26:29 +0100

    feat(application_load_balancer): Created ALB to route traffic to API.

--- main.tf
+++ main.tf
@@ -121,3 +121,44 @@ resource "aws_security_group" "ecs_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+# Create the Application Load Balancer.
+resource "aws_lb" "main" {
+  name                       = "ecsalb"
+  internal                   = false
+  load_balancer_type         = "application"
+  security_groups            = [aws_security_group.ecs_sg.id]
+  subnets                    = module.pagopa_pr_api_ms_vpc.public_subnets
+  idle_timeout               = 30
+  enable_deletion_protection = false
+}
+
+# Create the ALB target group.
+resource "aws_lb_target_group" "ecs_rest_api_tg" {
+  name        = "ecs-tg"
+  port        = 8000
+  protocol    = "HTTP"
+  vpc_id      = module.pagopa_pr_api_ms_vpc.vpc_id
+  target_type = "ip"
+
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 2
+    interval            = 10
+    matcher             = "200"
+  }
+}
+
+# Create the ALB listener.
+resource "aws_lb_listener" "alb_listener" {
+  load_balancer_arn = aws_lb.main.arn
+  port              = 80 # Can be redirected to 443 to improve security
+  protocol          = "HTTP"
+  default_action {
+    target_group_arn = aws_lb_target_group.ecs_rest_api_tg.arn
+    type             = "forward"
+  }
+}

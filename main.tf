22799e839f34010c247ce59e62f06c636dce9008
signorrayan <signor.rayan@gmail.com>
2024-02-03 21:17:46 +0100

    feat(aws_ecr_repository): Created ECR repositories for each microservice (FastAPI, Authentication, Authorization, Content).

--- main.tf
+++ main.tf
@@ -2,3 +2,20 @@
 resource "aws_s3_bucket" "logs_bucket" {
   bucket = "pr-microservices-logs-mhmdrza" # Change this to your desired bucket name
 }
+
+# ECR Repositories
+resource "aws_ecr_repository" "fastapi" {
+  name = "${var.general_name}-fastapi-service"
+}
+
+resource "aws_ecr_repository" "authentication" {
+  name = "${var.general_name}-authentication-service"
+}
+
+resource "aws_ecr_repository" "authorization" {
+  name = "${var.general_name}-authorization-service"
+}
+
+resource "aws_ecr_repository" "content" {
+  name = "${var.general_name}-content-service"
+}
